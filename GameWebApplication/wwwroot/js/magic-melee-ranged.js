475ec49a403a80963f3580763fd910623ca76b04
Brian Braine <bbraine95@gmail.com>
2023-01-03 21:34:37 -0500

    Changes to javascript file for magic melee ranged game

--- GameWebApplication/wwwroot/js/magic-melee-ranged.js
+++ GameWebApplication/wwwroot/js/magic-melee-ranged.js
@@ -1,15 +1,43 @@
-﻿function getUserChoice() {
+﻿let start = Date.now();
 
 
-    return userChoice;
+playTheGame =() => {
+    const magic = document.getElementById('#magic');
+    const melee = document.getElementById('#melee');
+    const ranged = document.getElementById('#ranged');
+
+    const userOptions = [magic, melee, ranged];
+
+    getPlayerChoice(userOptions);
+    getOpponentChoice();
+        
+    // Function that will detect which radio button (or image) is selected
+    getPlayerChoice = (userOptions) => {
+        let userOptions = document.getElementsByName('userChoice');
+        for (i = 0; i < userOptions.length; i++) {
+            if (userOptions[i].ariaChecked) {
+                document.getElementById("currentUserChoice").innerHTML = userOptions[i].ariaValueText; 
+            }   
+        }
+
+    }
+
+    // Function to generate and return a random selection for the opponent
+    getOpponentChoice = () => {
+        const opponentOptions = ['magic', 'melee', 'ranged'];  
+
+        let opponentIndex = Math.floor(Math.random() * 3);
+        let opponentChoice = opponentOptions[opponentIndex];
+
+        return opponentChoice;
+    }
+
+    let opponentChoice = getOpponentChoice();
+    console.log(opponentChoice);
 }
 
-function getComputerChoice() {
+playTheGame();
 
+let end = Date.now();
 
-    return computerChoice;
-}
-
-function compareChoices(userChoice, computerChoice) {
-
-}
+let timeElapsed = end - start;
